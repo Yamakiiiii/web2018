61a8afbd690d61a4aed457b648d3f23c54dfd36c
zm16555 <zm16555@josai.ac.jp>
2018-08-06 04:18:41 -0400

    message

---
+++ ex0-1.js
@@ -0,0 +1,16 @@
+ function fruit1(){
+  document.getElementByld('myImage').src='./image/father.png';}
+
+function fruit2(){
+  document.getElementByld('myImage').src='./image/mikan.png';}
+
+function fruit3(){
+  document.getElementByld('myImage').src='./image/melon.png';}
+
+
+
+
+  
+ 
+  
+ 
